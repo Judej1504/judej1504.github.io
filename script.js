27c227963fbe9457dca0ddb3194c863851fbd1c3
Judej1504 <judej1504@gmail.com>
2025-10-20 19:11:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,143 +1,6 @@
-var cmm = "0";
-var raw = 0;
-var m = "100m";
 const params = new URLSearchParams(window.location.search);
-var id = params.get("id") || "UC-9-kyTW8ZkZNDHQJ6FgpwQ";
-var url = "https://mixerno.space/api/youtube-channel-counter/user/";
-function spl(n) {
-	n = "" + n + "";
-	return n.split("", 9);
-}
-
-function settype(n) {
-	if (n < 1000000000) {
-		m = "100m";
-		document.querySelector("#odo1a").style.display = "inline-block";
-		document.querySelector("#odo2a").style.display = "inline-block";
-		document.querySelector("#odo3a").style.display = "inline-block";
-		document.querySelector("#odo4a").style.display = "inline-block";
-		document.querySelector("#odo5a").style.display = "inline-block";
-		document.querySelector("#odo6a").style.display = "inline-block";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "inline-block";
-		document.querySelector("#comma2a").style.display = "inline-block";
-	}
-	if (n < 100000000) {
-		m = "10m";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "inline-block";
-		document.querySelector("#odo3a").style.display = "inline-block";
-		document.querySelector("#odo4a").style.display = "inline-block";
-		document.querySelector("#odo5a").style.display = "inline-block";
-		document.querySelector("#odo6a").style.display = "inline-block";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "inline-block";
-		document.querySelector("#comma2a").style.display = "inline-block";
-	}
-	if (n < 10000000) {
-		m = "1m";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "inline-block";
-		document.querySelector("#odo4a").style.display = "inline-block";
-		document.querySelector("#odo5a").style.display = "inline-block";
-		document.querySelector("#odo6a").style.display = "inline-block";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "inline-block";
-		document.querySelector("#comma2a").style.display = "inline-block";
-	}
-	if (n < 1000000) {
-		m = "100k";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "none";
-		document.querySelector("#odo4a").style.display = "inline-block";
-		document.querySelector("#odo5a").style.display = "inline-block";
-		document.querySelector("#odo6a").style.display = "inline-block";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "none";
-		document.querySelector("#comma2a").style.display = "inline-block";
-	}
-	if (n < 100000) {
-		m = "10k";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "none";
-		document.querySelector("#odo4a").style.display = "none";
-		document.querySelector("#odo5a").style.display = "inline-block";
-		document.querySelector("#odo6a").style.display = "inline-block";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "none";
-		document.querySelector("#comma2a").style.display = "inline-block";
-	}
-	if (n < 10000) {
-		m = "1k";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "none";
-		document.querySelector("#odo4a").style.display = "none";
-		document.querySelector("#odo5a").style.display = "none";
-		document.querySelector("#odo6a").style.display = "inline-block";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "none";
-		document.querySelector("#comma2a").style.display = "inline-block";
-	}
-	if (n < 1000) {
-		m = "100";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "none";
-		document.querySelector("#odo4a").style.display = "none";
-		document.querySelector("#odo5a").style.display = "none";
-		document.querySelector("#odo6a").style.display = "none";
-		document.querySelector("#odo7a").style.display = "inline-block";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "none";
-		document.querySelector("#comma2a").style.display = "none";
-	}
-	if (n < 100) {
-		m = "10";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "none";
-		document.querySelector("#odo4a").style.display = "none";
-		document.querySelector("#odo5a").style.display = "none";
-		document.querySelector("#odo6a").style.display = "none";
-		document.querySelector("#odo7a").style.display = "none";
-		document.querySelector("#odo8a").style.display = "inline-block";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "none";
-		document.querySelector("#comma2a").style.display = "none";
-	}
-	if (n < 10) {
-		m = "1";
-		document.querySelector("#odo1a").style.display = "none";
-		document.querySelector("#odo2a").style.display = "none";
-		document.querySelector("#odo3a").style.display = "none";
-		document.querySelector("#odo4a").style.display = "none";
-		document.querySelector("#odo5a").style.display = "none";
-		document.querySelector("#odo6a").style.display = "none";
-		document.querySelector("#odo7a").style.display = "none";
-		document.querySelector("#odo8a").style.display = "none";
-		document.querySelector("#odo9a").style.display = "inline-block";
-		document.querySelector("#comma1a").style.display = "none";
-		document.querySelector("#comma2a").style.display = "none";
-	}
-	return m;
-}
+var id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
+var url = `https://ests.sctools.org/api/get/<id>`;
 
 const chart = new Highcharts.chart({
 	chart: {
@@ -146,8 +9,8 @@ const chart = new Highcharts.chart({
 		zoomType: "x",
 		backgroundColor: "transparent",
 		plotBorderColor: "transparent",
-		height: "153px",
-		animation: true,
+		height: "-11px",
+		animation: false,
 		style: {
 			fontFamily: "Roboto",
 		},
@@ -266,101 +129,31 @@ function getdata(a) {
 	fetch(url + a)
 		.then((res) => res.json())
 		.then((data) => {
-			cmm = spl(data.counts[0].count);
-			raw = data.counts[0].count;
-			document.getElementById("avatar").src = data.user[1].count;
-			document.getElementById("title").textContent = data.user[0].count;
+document.getElementById('c').innerHTML = data.estSubCount;	
+      document.getElementById("avatar").src = data.snippet.thumbnails.high.url;
+			document.getElementById("title").textContent = data.snippet.title;
 			if (chart.series[0].points.length >= 3600)
 				chart.series[0].data[0].remove();
-			chart.series[0].addPoint([Date.now(), data.counts[0].count]);
-			return cmm;
+			chart.series[0].addPoint([Date.now(), data.estSubCount]);
 		});
 }
 
-function render() {
-	settype(raw);
-	if (m == "100m") {
-		odo1a.innerHTML = cmm[0];
-		odo2a.innerHTML = cmm[1];
-		odo3a.innerHTML = cmm[2];
-		odo4a.innerHTML = cmm[3];
-		odo5a.innerHTML = cmm[4];
-		odo6a.innerHTML = cmm[5];
-		odo7a.innerHTML = cmm[6];
-		odo8a.innerHTML = cmm[7];
-		odo9a.innerHTML = cmm[8];
-	}
-	if (m == "10m") {
-		odo2a.innerHTML = cmm[0];
-		odo3a.innerHTML = cmm[1];
-		odo4a.innerHTML = cmm[2];
-		odo5a.innerHTML = cmm[3];
-		odo6a.innerHTML = cmm[4];
-		odo7a.innerHTML = cmm[5];
-		odo8a.innerHTML = cmm[6];
-		odo9a.innerHTML = cmm[7];
-	}
-	if (m == "1m") {
-		odo3a.innerHTML = cmm[0];
-		odo4a.innerHTML = cmm[1];
-		odo5a.innerHTML = cmm[2];
-		odo6a.innerHTML = cmm[3];
-		odo7a.innerHTML = cmm[4];
-		odo8a.innerHTML = cmm[5];
-		odo9a.innerHTML = cmm[6];
-	}
-	if (m == "100k") {
-		odo4a.innerHTML = cmm[0];
-		odo5a.innerHTML = cmm[1];
-		odo6a.innerHTML = cmm[2];
-		odo7a.innerHTML = cmm[3];
-		odo8a.innerHTML = cmm[4];
-		odo9a.innerHTML = cmm[5];
-	}
-	if (m == "10k") {
-		odo5a.innerHTML = cmm[0];
-		odo6a.innerHTML = cmm[1];
-		odo7a.innerHTML = cmm[2];
-		odo8a.innerHTML = cmm[3];
-		odo9a.innerHTML = cmm[4];
-	}
-	if (m == "1k") {
-		odo6a.innerHTML = cmm[0];
-		odo7a.innerHTML = cmm[1];
-		odo8a.innerHTML = cmm[2];
-		odo9a.innerHTML = cmm[3];
-	}
-	if (m == "100") {
-		odo7a.innerHTML = cmm[0];
-		odo8a.innerHTML = cmm[1];
-		odo9a.innerHTML = cmm[2];
-	}
-	if (m == "10") {
-		odo8a.innerHTML = cmm[0];
-		odo9a.innerHTML = cmm[1];
-	}
-	if (m == "1") {
-		odo9a.innerHTML = cmm[0];
-	}
-}
 setTimeout(() => {
 	getdata(id);
-	render();
 }, 100);
+
 setTimeout(() => {
 	getdata(id);
-	render();
 }, 500);
 setInterval(() => {
 	getdata(id);
-	render();
 }, 2000);
 
 function search() {
 	const prompt = window.prompt("Enter channel name, ID, or URL.");
 	if (prompt)
 		fetch(
-			`https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
+			`https://mixerno.space/api/youtube-channel-counter/search/${query}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
