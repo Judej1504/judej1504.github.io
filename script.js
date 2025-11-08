0dffea487c4682a39403374dbe8f0e0ca1c28332
Judej1504 <judej1504@gmail.com>
2025-11-08 22:09:04 +0000

    Fix API endpoint in search function

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ function search() {
 	const prompt = window.prompt("Enter channel name, ID, or URL.");
 	if (prompt)
 		fetch(
-			`https://subscribercount.app/api/search?platform=youtube&type=channel&query=${prompt}`
+			`https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
