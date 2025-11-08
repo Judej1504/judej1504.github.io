7752908b44bd6e872cdb20fd8d37299bc441faaf
Judej1504 <judej1504@gmail.com>
2025-11-08 22:06:12 +0000

    Fix API URL concatenation in search function

--- script.js
+++ script.js
@@ -153,7 +153,7 @@ function search() {
 	const prompt = window.prompt("Enter channel name, ID, or URL.");
 	if (prompt)
 		fetch(
-			`https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
+			`https://subscribercount.app/api/search?platform=youtube&type=channel&query=${prompt}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
