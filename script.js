9b0e57b6728e94919516812cbf0b0f9afd161b69
Judej1504 <judej1504@gmail.com>
2025-11-09 00:44:10 +0000

    Update script.js (changing api to mixerno.space

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 var id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
-var url = `https://ests.sctools.org/api/get/{id}`;
+var url = "https://mixerno.space/api/youtube-channel-counter/user/";
 
 const chart = new Highcharts.chart({
 	chart: {
@@ -153,7 +153,7 @@ function search() {
 	const prompt = window.prompt("Enter channel name, ID, or URL.");
 	if (prompt)
 		fetch(
-			`https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
+			`https://subscribercount.app/api/search?platform=youtube&type=channel&query=${prompt}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
