412e56580cc1d5125930f4cebf2fa99458f3bddd
Judej1504 <judej1504@gmail.com>
2025-11-09 01:01:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 var id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
-var url = `https://mixerno.space/api/youtube-channel-counter/user/${channelId}`;
+var url = `https://mixerno.space/api/youtube-channel-counter/user/${channelId}`; 
 
 const chart = new Highcharts.chart({
 	chart: {
