ac53adf5fb36e69c67dc4b2566eed2422fef3efa
Judej1504 <judej1504@gmail.com>
2025-11-09 00:46:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 var id = params.get("id") || "UCX6OQ3DkcsbYNE6H8uQQuVA";
-var url = "https://mixerno.space/api/youtube-channel-counter/user/";
+var url = `https://subscribercount.app/api/get?platform=youtube&type=channel&id=`;
 
 const chart = new Highcharts.chart({
 	chart: {
@@ -153,7 +153,7 @@ function search() {
 	const prompt = window.prompt("Enter channel name, ID, or URL.");
 	if (prompt)
 		fetch(
-			`https://subscribercount.app/api/search?platform=youtube&type=channel&query=${prompt}`
+			`https://axern.space/api/search?platform=youtube&type=channel&query=${prompt}`
 		)
 			.then((res) => res.json())
 			.then((data) => {
